fe52aee58c1335a344593bc1615fbf7355230424
Chetan Sengar <nathan4drake1999@gmail.com>
2022-04-20 13:54:17 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini-Message Board", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  const { author, message } = req.body;
+  console.log(author, message);
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = router;
